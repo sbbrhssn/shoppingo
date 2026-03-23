9b04fecb480c267ef207c5d3b9fc22f54850597e
Sabbir Hossain <sabbir1722013@gmail.com>
2026-03-23 15:20:19 +0600

    New signup route has been declared. from  Homepage signup to main signup page

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('/signup/', views.signup, name='signup'),
+    # path('vendors/', include('vendors.urls')),
     path('admin/', admin.site.urls),
 ]
